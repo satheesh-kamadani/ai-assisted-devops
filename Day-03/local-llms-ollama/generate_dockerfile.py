235e2d35743260a4e04003401066312c380ff4bb
Satheesh Kamadani <satheesh@Satheeshs-MacBook-Air.local>
2025-10-20 17:53:35 +0200

    Added documentation in readme file

---
+++ Day-03/local-llms-ollama/generate_dockerfile.py
@@ -0,0 +1,24 @@
+''' Generate docker file in different programing language using
+local llm model(llama3).
+'''
+import ollama
+
+PROMPT = """
+ONLY Generate an ideal Dockerfile for {language} with best practices. Do not provide any description
+Include:
+- Base image
+- Installing dependencies
+- Setting working directory
+- Adding source code
+- Running the application
+"""
+
+def generate_dockerfile(language):
+    response = ollama.chat(model= 'llama3.1:8b', message= [{'role': 'user', 'content': PROMPT.format(language=language)}])
+    return response['message']['content']
+
+if __name__ == '__main__':
+    language = input("Enter the programming language: ")
+    dockerfile = generate_dockerfile(language)
+    print("\nGenerated docker file:\n")
+    print(dockerfile)
